5a3588ef4129e51528a0e31b8a3ebc21d5c84452
frkofficial <farukakram2000@gmail.com>
2026-02-07 12:35:24 +0600

    math functions

---
+++ math.js
@@ -0,0 +1,30 @@
+
+
+const min = Math.min(33, 4, 100, 2, 200)
+
+console.log('Minimum number is: ',min)
+
+const max =Math.max(10, 22, 40, 300, -20, 60, 100, -300, 8, 9)
+
+console.log('Max number is: ',max)
+
+// show the pi value
+console.log(Math.PI)
+
+
+// ignore the sign
+console.log(Math.abs(-88))
+
+// show integer
+
+console.log(Math.round(2.5))
+
+// floor
+
+console.log(Math.floor(2.555))
+
+
+// random
+
+console.log(Math.random())
+console.log(Math.random() * 10)
